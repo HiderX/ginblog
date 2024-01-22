b14c6c18ee7f889f99f3af55cd7ba7f1d19342f7
hiderx <3193997905@qq.com>
2024-01-22 14:35:17 +0800

    initialize

---
+++ utils/settings.go
@@ -0,0 +1,37 @@
+package utils
+
+import "gopkg.in/ini.v1"
+
+var (
+	AppMode    string
+	HttpPort   string
+	Db         string
+	DbHost     string
+	DbPort     string
+	DbUser     string
+	DbPassWord string
+	Dbname     string
+)
+
+func init() {
+	file, err := ini.Load("config/config.ini")
+	if err != nil {
+		panic(err)
+	}
+	LoadServer(file)
+	LoadData(file)
+}
+
+func LoadServer(file *ini.File) {
+	AppMode = file.Section("server").Key("AppMode").MustString("debug")
+	HttpPort = file.Section("server").Key("HttpPort").MustString(":3000")
+}
+
+func LoadData(file *ini.File) {
+	Db = file.Section("database").Key("Db").MustString("mysql")
+	DbHost = file.Section("database").Key("DbHost").MustString("localhost")
+	DbPort = file.Section("database").Key("DbPort").MustString("3306")
+	DbUser = file.Section("database").Key("DbUser").MustString("ginblog")
+	DbPassWord = file.Section("database").Key("DbPassWord").MustString("aadmin123")
+	Dbname = file.Section("database").Key("Dbname").MustString("ginblog")
+}
