cc2980bbf8248815b311ce8556552afbf1aa4626
hiderx <3193997905@qq.com>
2024-01-24 14:36:00 +0800

    CRUD User

--- api/v1/user.go
+++ api/v1/user.go
@@ -1,25 +1,73 @@
 package v1
 
 import (
+	"ginblog/model"
+	"ginblog/utils"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 func UserExist(c *gin.Context) {
-
+	username := c.Query("username")
+	code := model.CheckUser(username)
+	c.JSON(200, gin.H{
+		"status":  code,
+		"message": utils.GetErrMsg(code),
+	})
 }
 
 func AddUser(c *gin.Context) {
-
+	var data model.User
+	_ = c.ShouldBindJSON(&data)
+	code := model.CheckUser(data.Username)
+	if code == utils.SUCCESS {
+		model.CreateUser(&data)
+	}
+	if code == utils.ERROR_USERNAME_USED {
+		code = utils.ERROR_USERNAME_USED
+	}
+	c.JSON(200, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": utils.GetErrMsg(code),
+	})
 }
 
 func GetUsers(c *gin.Context) {
-
+	pageSize, _ := strconv.Atoi(c.Query("pageSize"))
+	pageNum, _ := strconv.Atoi(c.Query("pageNum"))
+	if pageSize == 0 {
+		pageSize = -1
+	}
+	if pageNum == 0 {
+		pageNum = -1
+	}
+	data := model.GetUsers(pageSize, pageNum)
+	code := utils.SUCCESS
+	c.JSON(200, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": utils.GetErrMsg(code),
+	})
 }
 
 func EditUser(c *gin.Context) {
-
+	id, _ := strconv.Atoi(c.Params.ByName("id"))
+	var data model.User
+	_ = c.ShouldBindJSON(&data)
+	code := model.EditUser(id, &data)
+	c.JSON(200, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": utils.GetErrMsg(code),
+	})
 }
 
 func DeleteUser(c *gin.Context) {
-
+	id, _ := strconv.Atoi(c.Params.ByName("id"))
+	code := model.DeleteUser(id)
+	c.JSON(200, gin.H{
+		"status":  code,
+		"message": utils.GetErrMsg(code),
+	})
 }
--- model/User.go
+++ model/User.go
@@ -1,10 +1,61 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"errors"
+	"ginblog/utils"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
 	Username string `gorm:"type:varchar(20);not null" json:"username"`
 	Password string `gorm:"type:varchar(20);not null" json:"password"`
 	Role     int    `gorm:"type:int;DEFAULT:1" json:"role"`
+}
+
+func CreateUser(data *User) int {
+	err := Db.Create(&data).Error
+	if err != nil {
+		return utils.ERROR
+	}
+	return utils.SUCCESS
+}
+
+func CheckUser(username string) int {
+	var user User
+	Db.Select("id").Where("username = ?", username).First(&user)
+	if user.ID > 0 {
+		return utils.ERROR_USERNAME_USED
+	}
+	return utils.SUCCESS
+}
+
+func GetUsers(pageSize int, pageNum int) []User {
+	var users []User
+	err := Db.Limit(pageSize).Offset((pageNum - 1) * pageSize).Find(&users).Error
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil
+	}
+	return users
+}
+
+func EditUser(id int, data *User) int {
+	var user User
+	var maps = make(map[string]interface{})
+	maps["username"] = data.Username
+	maps["role"] = data.Role
+	err := Db.Model(&user).Where("id = ?", id).Updates(maps).Error
+	if err != nil {
+		return utils.ERROR
+	}
+	return utils.SUCCESS
+}
+
+func DeleteUser(id int) int {
+	var user User
+	err := Db.Where("id = ?", id).Delete(&user).Error
+	if err != nil {
+		return utils.ERROR
+	}
+	return utils.SUCCESS
 }
